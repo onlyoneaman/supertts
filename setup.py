bb2f57ef51fb4df59e6014eeb378f25bd246f429
onlyoneaman <2000.aman.sinha@gmail.com>
2024-02-26 16:31:08 +0530

    v0.0.6

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='supertts',
-    version='0.0.4',
+    version='0.0.6',
     author='Aman Kumar',
     author_email='2000.aman.sinha@gmail.com',
-    packages=find_packages(),
-    install_requires=[
-        'openai==1.12.0'
+    requires=[
+        'openai',
     ],
+    packages=find_packages(),
     description='A super TTS package supporting multiple providers.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
