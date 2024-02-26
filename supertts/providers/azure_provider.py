f48f80e755bb5c0c1c091a17520ab5b5de77355d
onlyoneaman <2000.aman.sinha@gmail.com>
2024-02-27 01:49:40 +0530

    Azure TTS

--- supertts/providers/azure_provider.py
+++ supertts/providers/azure_provider.py
@@ -15,8 +15,34 @@ class AzureProvider:
         self.subscription_key = subscription_key
         self.region = region
 
-    def speak(self, text):
-        pass
+    def speak(
+        self, 
+        text,
+        voice_name=None,
+        language="en-US"
+    ):
+        speech_config = speechsdk.SpeechConfig(
+            subscription=self.subscription_key, 
+            region=self.region
+        )
+
+        speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config)
+
+        speech_config.speech_synthesis_language = language
+        if voice_name:
+            speech_config.speech_synthesis_voice_name = voice_name
+
+        result = speech_synthesizer.speak_text_async(text).get()
+
+        if result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
+            return result
+        elif result.reason == speechsdk.ResultReason.Canceled:
+            cancellation_details = result.cancellation_details
+            print(f"Speech synthesis canceled: {cancellation_details.reason}")
+            if cancellation_details.reason == speechsdk.CancellationReason.Error:
+                print(f"Error details: {cancellation_details.error_details}")
+                raise ValueError(f"Error details: {cancellation_details.error_details}")
+            raise ValueError(f"Speech synthesis canceled: {cancellation_details.reason}")
 
     def available_voices(self):
         voices = []
