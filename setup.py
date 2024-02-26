93a2fa2ad6a0cb5619eee820e40efff8e8410c45
onlyoneaman <2000.aman.sinha@gmail.com>
2024-02-26 20:32:10 +0530

    v0.0.12

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='supertts',
-    version='0.0.11',
+    version='0.0.12',
     author='Aman Kumar',
     author_email='2000.aman.sinha@gmail.com',
     install_requires=[
