d37c35ad70245220abaddc9ae17bc914d82f6b45
onlyoneaman <2000.aman.sinha@gmail.com>
2024-02-27 01:10:00 +0530

    Azure v0.0.13

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='supertts',
-    version='0.0.12',
+    version='0.0.13',
     author='Aman Kumar',
     author_email='2000.aman.sinha@gmail.com',
     install_requires=[
