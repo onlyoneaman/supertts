0e96d2f696d23f7a04b4f73c21d85e949d8eb876
onlyoneaman <2000.aman.sinha@gmail.com>
2024-02-28 23:14:05 +0530

    update setup to v0.0.18

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='supertts',
-    version='0.0.17',
+    version='0.0.18',
     author='Aman Kumar',
     author_email='2000.aman.sinha@gmail.com',
     install_requires=[
